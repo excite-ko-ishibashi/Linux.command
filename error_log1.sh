1e77d02f90b282affb777dbf991befd7d39b640c
kotaro ishibashi <kotaro.ishibashi@excite.jp>
2021-07-30 13:12:13 +0900

    recipe0xのエラーログ抽出(引数Ver)

--- error_log1.sh
+++ error_log1.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
+grep -E 'HTTP/1\.[0,1]" [4-9]{1}[0-9]{2}' $1 | grep -Ev '\.(js|css|png|jpg|jpeg|gif)' > error_log_all1.txt
+grep -E 'HTTP/1\.[0,1]" [4-9]{1}[0-9]{2}' $2 | grep -Ev '\.(js|css|png|jpg|jpeg|gif)' >> error_log_all1.txt
+grep -E 'HTTP/1\.[0,1]" [4-9]{1}[0-9]{2}' $3 | grep -Ev '\.(js|css|png|jpg|jpeg|gif)' >> error_log_all1.txt
 
-grep -v 'HTTP/1.1" 200' recipe01_access_log.20210715.txt | grep -v 'HTTP/1.0" 200' | grep -v 'HTTP/1.1" 206' | grep -v 'HTTP/1.1" 301' | grep -v 'HTTP/1.1" 302' | grep -v .png | grep -v .jpg | grep -v .jpeg | grep -v .css | grep -v .js >> error_log_all.txt 
-grep -v 'HTTP/1.1" 200' recipe02_access_log.20210715.txt | grep -v 'HTTP/1.0" 200' | grep -v 'HTTP/1.1" 206' | grep -v 'HTTP/1.1" 301' | grep -v 'HTTP/1.1" 302' | grep -v .png | grep -v .jpg | grep -v .jpeg | grep -v .css | grep -v .js >> error_log_all.txt
-grep -v 'HTTP/1.1" 200' recipe03_access_log.20210715.txt | grep -v 'HTTP/1.0" 200' | grep -v 'HTTP/1.1" 206' | grep -v 'HTTP/1.1" 301' | grep -v 'HTTP/1.1" 302' | grep -v .png | grep -v .jpg | grep -v .jpeg | grep -v .css | grep -v .js >> error_log_all.txt
-wc -l error_log_all.txt
+cut -d "\"" -f 2 error_log_all1.txt | grep 'excite' >> error_log_recipe1.txt
 
-grep http://erecipe.woman.excite.co.jp/search error_log_all.txt >> error_log_recipe.txt
-wc -l error_log_recipe.txt
+cut -f 2- -d "/" error_log_recipe1.txt | cut -f 1 -d "H" >> error_log_recipe_path1.txt
 
-cut -f 4- -d "/" error_log_recipe.txt | cut -f 1 -d "H" >> error_log_recipe_path.txt
-wc -l error_log_recipe_path.txt
+grep -v 'login' error_log_recipe_path1.txt >> error_log_recipe_path11.txt
 
-sort error_log_recipe_path.txt >> error_log_recipe_sortpath.txt
-wc -l error_log_recipe_sortpath.txt
+awk 'BEGIN{FS="&title="}{print $1}' error_log_recipe_path11.txt | awk 'BEGIN{FS="\\?_s="}{print $1}' | awk 'BEGIN{FS="\\?returnUrl"}{print $1}' | awk 'BEGIN{FS="\\?url="}{print $1}' >> error_log_recipe_path21.txt
 
+sort error_log_recipe_path21.txt | uniq -c | sort >> error_log_recipe_sortpath1.txt
